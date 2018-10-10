f22b7cfd910a57501088e95da106ab798c33f2ab
Dylan Sheffer <dsheffer16@gmail.com>
2018-10-10 17:34:27 -0400

    Navigation Fixed

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,41 +9,43 @@ export class Navigation extends Component {
     const { children, className, navItems, id, beforeItems } = this.props;
     return (
       <nav id={id} className={className}>
-        {beforeItems}
-        <ul>
-          <li><Link to="/">
-            <div className="nav-item--content">
-              <FontAwesomeIcon className="nav-item--icon" icon="home" />
-              Home
-            </div>
-          </Link></li>
-          <li><Link to="/about">
-            <div className="nav-item--content">
-              <FontAwesomeIcon className="nav-item--icon" icon="user" />
-              About
-            </div>
-          </Link></li>
-          <li><Link to="/work">
-            <div className="nav-item--content">
-              <FontAwesomeIcon className="nav-item--icon" icon="code" />
-              Work
-            </div>
-          </Link></li>
-          <li><Link to="/talks">
-            <div className="nav-item--content">
-              <FontAwesomeIcon className="nav-item--icon" icon="chalkboard-teacher" />
-              Talks
-            </div>
-          </Link></li>
-          <li><Link to="/blog">
-            <div className="nav-item--content">
-              <FontAwesomeIcon className="nav-item--icon" icon="pencil-alt" />
-              Blog
-            </div>
-          </Link></li>
-          {navItems}
-        </ul>
-        {children}
+        <div className="nav-content">
+          {beforeItems}
+          <ul>
+            <li><Link to="/">
+              <div className="nav-item--content">
+                <FontAwesomeIcon className="nav-item--icon" icon="home" />
+                Home
+              </div>
+            </Link></li>
+            <li><Link to="/about">
+              <div className="nav-item--content">
+                <FontAwesomeIcon className="nav-item--icon" icon="user" />
+                About
+              </div>
+            </Link></li>
+            <li><Link to="/work">
+              <div className="nav-item--content">
+                <FontAwesomeIcon className="nav-item--icon" icon="code" />
+                Work
+              </div>
+            </Link></li>
+            <li><Link to="/talks">
+              <div className="nav-item--content">
+                <FontAwesomeIcon className="nav-item--icon" icon="chalkboard-teacher" />
+                Talks
+              </div>
+            </Link></li>
+            <li><Link to="/blog">
+              <div className="nav-item--content">
+                <FontAwesomeIcon className="nav-item--icon" icon="pencil-alt" />
+                Blog
+              </div>
+            </Link></li>
+            {navItems}
+          </ul>
+          {children}
+        </div>
       </nav>
     )
   }
