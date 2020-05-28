63485eee8ce5086cd60cf257f12d73680ec9daec
Dylan Sheffer <dsheffer16@gmail.com>
2020-05-28 10:54:15 -0400

    fix: git not picking up on case change of file name

---
+++ src/components/mdxComponents/Code.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Code = styled.code`
+  background: var(--surface-dark);
+  border: 0.25rem solid var(--black);
+  border-radius: 0.5rem;
+  padding: 0.25rem 0.5rem;
+  font-size: 0.75em;
+  font-family: var(--monospace);
+`;
