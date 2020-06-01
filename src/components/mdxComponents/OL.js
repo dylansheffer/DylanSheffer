4d609abb3f05149d4c34a3ae5475b8260c0a5d80
Dylan Sheffer <dsheffer16@gmail.com>
2020-06-01 17:44:07 -0400

    fix: add margins in nested lists

--- src/components/mdxComponents/OL.js
+++ src/components/mdxComponents/OL.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 export const OL = styled.ol`
+  ul,
+  ol {
+    margin: 1.5rem;
+  }
   li {
     margin-bottom: 1.5rem;
   }
--- src/components/mdxComponents/UL.js
+++ src/components/mdxComponents/UL.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 export const UL = styled.ul`
+  ul,
+  ol {
+    margin: 1.5rem;
+  }
   li {
     margin-bottom: 1.5rem;
   }
